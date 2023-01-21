39a43fa9f48e8771ee2dd3967025d2877d7dc5ec
jackesfonseca <jackesdafonseca@gmail.com>
2023-01-21 19:31:38 -0300

    alterando método HTTP

--- src/main/java/com/erifas/backend/resource/controller/RifaController.java
+++ src/main/java/com/erifas/backend/resource/controller/RifaController.java
@@ -3,10 +3,7 @@ package com.erifas.backend.resource.controller;
 import com.erifas.backend.service.RifaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/rifa")
@@ -18,7 +15,7 @@ public class RifaController {
         this.rifaService = rifaService;
     }
 
-    @PostMapping("/aprovar/{id}")
+    @PutMapping("/aprovar/{id}")
     public ResponseEntity<RifaService> aprovarRifa(@PathVariable Long id)
     {
        if(rifaService.aprovarRifa(id))
@@ -28,7 +25,7 @@ public class RifaController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    @PostMapping("/cancelar/{id}")
+    @PutMapping("/cancelar/{id}")
     public ResponseEntity<RifaService> cancelarRifa(@PathVariable Long id)
     {
         if(rifaService.cancelarRifa(id))
