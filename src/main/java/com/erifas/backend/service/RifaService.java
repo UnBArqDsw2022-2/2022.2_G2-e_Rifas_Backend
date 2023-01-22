902cf980fb45c526fa5614a8ec71d9f7102e9b95
EuricoAbreu <euricoabreuneto@hotmail.com>
2023-01-22 15:09:07 -0300

    Alteração na service

--- src/main/java/com/erifas/backend/service/RifaService.java
+++ src/main/java/com/erifas/backend/service/RifaService.java
@@ -3,6 +3,8 @@ package com.erifas.backend.service;
 import com.erifas.backend.constants.StatusRifa;
 import com.erifas.backend.persistence.model.Rifa;
 import com.erifas.backend.repository.jpa.core.RifaRepository;
+import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
+
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,11 +18,9 @@ public class RifaService {
         this.rifaRepository = rifaRepository;
     }
 
-    public boolean aprovarRifa (Long id)
-    {
+    public boolean aprovarRifa(Long id) {
         Optional<Rifa> rifa = rifaRepository.findById(id);
-        if(rifa.isPresent())
-        {
+        if (rifa.isPresent()) {
             rifa.get().setStatus(StatusRifa.ABERTA);
             rifaRepository.save(rifa.get());
             return true;
@@ -28,15 +28,24 @@ public class RifaService {
         return false;
     }
 
-    public boolean cancelarRifa (Long id)
-    {
+    public boolean cancelarRifa(Long id) {
         Optional<Rifa> rifa = rifaRepository.findById(id);
-        if(rifa.isPresent())
-        {
+        if (rifa.isPresent()) {
             rifa.get().setStatus(StatusRifa.CANCELADA);
             rifaRepository.save(rifa.get());
             return true;
         }
         return false;
+    }
+
+    public Optional<Rifa> findById(Long id) {
+        return rifaRepository.findById(id);
+    }
+
+    public Optional<Integer> getMaxBilhetes(Long idRifa) {
+        Optional<Rifa> rifa = rifaRepository.findById(idRifa);
+        if (rifa.isPresent())
+            return Optional.of(rifa.get().getMaximoBilhetes());
+        return Optional.empty();
     }
 }
