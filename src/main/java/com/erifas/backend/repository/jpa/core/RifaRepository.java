2480c3835640b450c043a96c79d94d758e481234
João Paulo Lima <jpaulo.on@gmail.com>
2023-01-22 02:05:47 -0300

    :sparkles: Adicionando função para encontrar rifas conforme o status.

--- src/main/java/com/erifas/backend/repository/jpa/core/RifaRepository.java
+++ src/main/java/com/erifas/backend/repository/jpa/core/RifaRepository.java
@@ -1,9 +1,14 @@
 package com.erifas.backend.repository.jpa.core;
 
+import java.util.List;
+
+import com.erifas.backend.constants.StatusRifa;
 import com.erifas.backend.persistence.model.Rifa;
 import com.erifas.backend.repository.BaseJpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RifaRepository extends BaseJpaRepository<Rifa, Long> {
+
+    List<Rifa> findByStatus(StatusRifa statusRifa);
 }
