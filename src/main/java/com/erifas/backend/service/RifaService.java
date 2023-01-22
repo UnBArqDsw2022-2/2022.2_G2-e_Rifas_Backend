4ead49f76d63dd9544e1a813a91954e03416f930
João Paulo Lima <jpaulo.on@gmail.com>
2023-01-22 02:07:52 -0300

    :sparkles: Adicionando função para encontrar rifas abertas no RifaService.

--- src/main/java/com/erifas/backend/service/RifaService.java
+++ src/main/java/com/erifas/backend/service/RifaService.java
@@ -1,26 +1,31 @@
 package com.erifas.backend.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.erifas.backend.constants.StatusRifa;
+import com.erifas.backend.external.keycloak.service.KeycloakTokenComponent;
 import com.erifas.backend.persistence.model.Rifa;
 import com.erifas.backend.repository.jpa.core.RifaRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class RifaService {
 
     private final RifaRepository rifaRepository;
+    Logger logger = LoggerFactory.getLogger(KeycloakTokenComponent.class);
 
     public RifaService(RifaRepository rifaRepository) {
         this.rifaRepository = rifaRepository;
     }
 
-    public boolean mudarStatusRifa (Long id, StatusRifa status)
-    {
+    public boolean mudarStatusRifa(Long id, StatusRifa status) {
         Optional<Rifa> rifa = rifaRepository.findById(id);
-        if(rifa.isPresent())
-        {
+        if (rifa.isPresent()) {
             switch (status) {
                 case ABERTA -> rifa.get().setStatus(StatusRifa.ABERTA);
                 case FECHADA -> rifa.get().setStatus(StatusRifa.FECHADA);
@@ -31,5 +36,21 @@ public class RifaService {
             return true;
         }
         return false;
+    }
+
+    public ResponseEntity<List<Rifa>> rifasAbertas() {
+        try {
+            List<Rifa> rifasAbertas = rifaRepository.findByStatus(StatusRifa.ABERTA);
+            if (!rifasAbertas.isEmpty()) {
+                logger.info("Rifas encontradas: ");
+                return new ResponseEntity<>(rifasAbertas, HttpStatus.OK);
+            } else {
+                logger.error("Nenhuma rifa encontrada!");
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        } catch (Exception e) {
+            logger.error("Erro ao encontrar rifas abertas!", e);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
