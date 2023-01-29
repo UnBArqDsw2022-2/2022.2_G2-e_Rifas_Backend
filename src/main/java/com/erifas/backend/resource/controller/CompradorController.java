4a1d588de91babe27852c87e591b5e0898ba7c38
ErickMVdO <erickmelovidal@gmail.com>
2023-01-29 14:50:47 -0300

    corrigindo imports
    Co-authored-by: André Alves <andremralves@gmail.com>

--- src/main/java/com/erifas/backend/resource/controller/CompradorController.java
+++ src/main/java/com/erifas/backend/resource/controller/CompradorController.java
@@ -3,7 +3,10 @@ package com.erifas.backend.resource.controller;
 import com.erifas.backend.persistence.model.Comprador;
 import com.erifas.backend.service.CompradorService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/comprador")
