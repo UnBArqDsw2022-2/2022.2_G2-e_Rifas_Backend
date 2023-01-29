cb09f343684fb312f21da119650b4e9e44342e44
ErickMVdO <erickmelovidal@gmail.com>
2023-01-29 16:13:01 -0300

    adicionando import do CrossOrigin

--- src/main/java/com/erifas/backend/resource/controller/CompradorController.java
+++ src/main/java/com/erifas/backend/resource/controller/CompradorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/comprador")
