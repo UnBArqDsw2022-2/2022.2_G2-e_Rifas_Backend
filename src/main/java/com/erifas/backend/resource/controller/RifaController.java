b295155baddc727170f401d31907411b7ba50dee
dev-brito <britoguilherme408@gmail.com>
2023-01-30 23:27:47 -0300

    Fixing Imports

--- src/main/java/com/erifas/backend/resource/controller/RifaController.java
+++ src/main/java/com/erifas/backend/resource/controller/RifaController.java
@@ -1,16 +1,15 @@
 package com.erifas.backend.resource.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import com.erifas.backend.constants.StatusRifa;
 import com.erifas.backend.persistence.model.Rifa;
 import com.erifas.backend.service.RifaService;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rifa")
